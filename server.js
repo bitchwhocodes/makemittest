6fc825fa784ef566a17762cb90b5e4301ff31ffa
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2015-02-28 21:07:48 -0500

    body parser

--- server.js
+++ server.js
@@ -4,19 +4,24 @@ var app = express();
 var port = process.env.PORT || 1337;
 
 var log = "";
-
+app.use(express.bodyParser());
 app.get('/webhook_url', function (req, res) {
-   
-      res.setHeader('Content-Type', 'application/json');
-    res.end(request);
-    //res.writeHead(200, { 'Content-Type': 'text/plain' });
-    //res.end(req.query.venmo_challenge);
+    
+    if(request.body){
+         res.setHeader('Content-Type', 'application/json');
+    res.end(request.body);
+}else{
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(req.query.venmo_challenge);
+}
+    
 });
 
 app.post('/webhook_url',function(req,res){
 
     res.setHeader('Content-Type', 'application/json');
-    res.end(request);
+    res.end(request.body);
+
 });
 
 
