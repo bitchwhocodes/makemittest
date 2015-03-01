51866929c122c1e9f1f0c92fa1b56813d0fccd46
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2015-02-28 20:54:13 -0500

    added in a post request

---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 1337;
+
+var log = "";
+
+app.get('/webhook_url', function (req, res) {
+   
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(req.query.venmo_challenge);
+});
+
+app.post('/webhook_url',function(req,res){
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(request);
+});
+
+
+app.get('/', function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello world");
+});
+    
+
+app.get('/test', function(req, res) {
+   
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello world");
+});
+
+app.post('/',function(req,res){
+
+    res.setHeader('Content-Type', 'application/json');
+    res.end(request);
+});
+
+
+app.get('/arduino', function(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("01,02\n");
+});
+
+var server = app.listen(port, function () { });
